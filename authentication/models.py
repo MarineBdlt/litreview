152122293d8952bed78f3ef2f47a1ab196e20dbd
Marine BD <marine.bap-deletang@aum.bio>
2022-01-28 18:45:46 +0100

    add models in db

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
+from django.db import models
 
-# Create your models here.
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following",
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
+
+    class Meta:
+        unique_together = ("user", "followed_user")
--- flux/models.py
+++ flux/models.py
@@ -1,3 +1,31 @@
+from tkinter import CASCADE
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.forms import DateTimeField
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    title = models.fields.CharField(max_length=128)
+    description = models.fields.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+
+
+# migrer les changements
+# ajouter connexion
+# ajouter pages de base
